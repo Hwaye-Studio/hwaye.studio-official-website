1dab67815c4cd5a9c939e5e79554b3fd176027b7
retinam2 <ilham.meidi@gmail.com>
2026-09-09 16:40:43 +0700

    add static files htmll

---
+++ out/text/brand.js
@@ -0,0 +1,17 @@
+module.exports = {
+  starter: {
+    name: 'Hwaye Studio',
+    desc: 'Some stories don\'t scream. They settle into the cracks of who you were.',
+    prefix: 'hwaye',
+    footerText: 'Hwaye Studio All Rights Reserved 2026',
+    logoText: 'Hwaye Studio',
+    projectName: 'Hwaye Studio',
+    url: 'http://hwaye-studio.com',
+    img: '/images/hwaye-studio-logo.png',
+    notifMsg: 'Some stories don\'t scream. They settle into the cracks of who you were.',
+    discordLink: 'https://discord.gg/NHmQ7cqmkS',
+    youtubeLink: 'https://youtube.com/@hwaye_ch',
+    tiktokLink: 'https://tiktok.com/@hwaye_ch',
+    instagram: 'https://instagram.com/hwaye_ch'
+  }
+};
